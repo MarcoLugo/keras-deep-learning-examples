5bddb83d5f9cfcf4305a39ddd3d7282d314a5f54
MarcoLugo <malr87@hotmail.com>
2018-01-31 17:01:46 -0500

    extend data partitioning to images with more than one instance of a shape

--- recreate_data.sh
+++ recreate_data.sh
@@ -2,3 +2,4 @@
 
 rm -rf input
 python3 create_images.py 128 1800 2
+python3 split_data.py 0.2
--- split_data.py
+++ split_data.py
@@ -10,29 +10,30 @@ import shutil
 
 random.seed(404) # Set seed for reproducibility
 shape_list = ['circle', 'rectangle']
+dir_appendix = ['', '_n']
 validation_ratio = float(sys.argv[1])
 
+for d in dir_appendix:
+    for subdir, dirs, files in os.walk('./input/images'+d):
+        for fname in files:
+            fullname = os.path.join(subdir, fname)
 
-for subdir, dirs, files in os.walk('./input/images'):
-    for fname in files:
-        fullname = os.path.join(subdir, fname)
+            source_path_img = subdir
+            source_path_mask = subdir.replace('images'+d, 'masks'+d)
 
-        source_path_img = subdir
-        source_path_mask = subdir.replace('images', 'masks')
+            if random.random() < validation_ratio:
+                dest_path_img = source_path_img.replace('input', 'input/validation'+d)
+                dest_path_mask = source_path_mask.replace('input', 'input/validation'+d)
+            else:
+                dest_path_img = source_path_img.replace('input', 'input/train'+d)
+                dest_path_mask = source_path_mask.replace('input', 'input/train'+d)
 
-        if random.random() < validation_ratio:
-            dest_path_img = source_path_img.replace('input', 'input/validation')
-            dest_path_mask = source_path_mask.replace('input', 'input/validation')
-        else:
-            dest_path_img = source_path_img.replace('input', 'input/train')
-            dest_path_mask = source_path_mask.replace('input', 'input/train')
+            # Create directories if missing
+            if not os.path.exists(dest_path_img):
+                os.makedirs(dest_path_img)
+            if not os.path.exists(dest_path_mask):
+                os.makedirs(dest_path_mask)
 
-        # Create directories if missing
-        if not os.path.exists(dest_path_img):
-            os.makedirs(dest_path_img)
-        if not os.path.exists(dest_path_mask):
-            os.makedirs(dest_path_mask)
-
-        # Copy files to new directories
-        shutil.copy(os.path.join(source_path_img, fname), os.path.join(dest_path_img, fname))
-        shutil.copy(os.path.join(source_path_mask, fname), os.path.join(dest_path_mask, fname))
+            # Copy files to new directories
+            shutil.copy(os.path.join(source_path_img, fname), os.path.join(dest_path_img, fname))
+            shutil.copy(os.path.join(source_path_mask, fname), os.path.join(dest_path_mask, fname))
